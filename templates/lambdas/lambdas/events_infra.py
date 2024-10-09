c8a66521b8353f06dc93c673d8853f1402fb3848
David De Los Santos Cuy Sánchez <david.cuy.sanchez@gmail.com>
2024-10-09 12:40:51 -0600

    feat: implements force update ecs at push ecr

---
+++ templates/lambdas/lambdas/events_infra.py
@@ -0,0 +1,31 @@
+import boto3
+import botocore
+import traceback
+
+def handler(event, context):
+    print("Received event: " + str(event))
+    ecs_cluster = event['ECSClusterName']
+    ecs_service = event['ECSServiceName']
+    response = {"requestId": context.aws_request_id, "status": "success"}
+    
+    try:
+        ecs = boto3.client('ecs')
+        response["updateServiceResponse"] = ecs.update_service(
+            cluster=ecs_cluster,
+            service=ecs_service,
+            forceNewDeployment=True
+        )
+    except botocore.exceptions.ClientError as e:
+        traceback.print_exc()
+        response["status"] = "failure"
+        response["errorDetails"] = "Failed to update ecs service by client error"
+        response["errorMessage"] = str(e)
+
+    except Exception as e:
+        traceback.print_exc()
+        response["status"] = "failure"
+        response["errorDetails"] = "General Fail"
+        response["errorMessage"] = str(e)
+    
+    print(response)
+    return response
